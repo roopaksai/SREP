00ef0189c9f185212b6e3e4f2c676f515573f03f
Roopak <sesikala@gitam.in>
2024-12-07 15:47:27 +0530

    Refactor home route to use Flask-RESTful for improved API structure

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-
+from flask_restful import Api, Resource
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello! This is my first Flask web app!"
-
+api = Api(app)
+class Home(Resource):
+    def get(self):
+        return {"message": "Hello! This is my first Flask web app!"}
+api.add_resource(Home, "/")
 if __name__ == "__main__":
     app.run(debug=True)
 
